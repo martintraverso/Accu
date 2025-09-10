b45f453c8a9fbacd379ae2cede150a6a01046b05
Martin Traverso <martin@traverso.com.uy>
2025-09-09 23:53:31 -0300

    Add simple factory for different http methods

--- src/main/java/com/martintraverso/AccuWeather/AccuWeatherCall.java
+++ src/main/java/com/martintraverso/AccuWeather/AccuWeatherCall.java
@@ -7,8 +7,8 @@ import com.martintraverso.AccuWeather.Response.AccuWeatherResponse;
 import com.martintraverso.AccuWeather.Response.FailedResponse;
 import com.martintraverso.AccuWeather.Response.Location.AdminArea;
 import com.martintraverso.AccuWeather.Response.Location.AdminAreaListResponse;
+import com.martintraverso.AccuWeather.Utilities.HttpRequestFactory;
 
-import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -18,8 +18,6 @@ public class AccuWeatherCall {
 
     private final HttpClient client;
     private final ObjectMapper mapper;
-    String baseUrl = "http://dataservice.accuweather.com/";
-    String apiKey = System.getenv("ACCUWEATHER_API_KEY");
 
     public AccuWeatherCall() {
         client = HttpClient.newHttpClient();
@@ -28,28 +26,11 @@ public class AccuWeatherCall {
 
     public AccuWeatherResponse call(AccuWeatherRequest accuWeatherRequest) {
         try {
-            if (apiKey == null) {
-                throw new IllegalStateException("ACCUWEATHER_API_KEY environment variable is required");
-            }
-
-            String endpoint = baseUrl + accuWeatherRequest.getEndpoint() + "?apikey=" + apiKey;
-
-            // @todo I need to work in different http methods.
-            // I do have the method in the Request object.
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(endpoint))
-                    .GET()
-                    .header("Accept", "*/*")
-                    .header("Accept-Encoding", "identity")
-                    .header("Accept-Language", "es-ES")
-                    .build();
-
+            HttpRequest request = HttpRequestFactory.create(accuWeatherRequest);
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-            // @todo this is a level elevated from wrong, but it is working in this first call.
-            // Need to understand what to make this correctly.
             if (response.statusCode() == 200) {
-                List<AdminArea> ListOfResponses = mapper.readValue(response.body(), new TypeReference<List<AdminArea>>() {
+                List<AdminArea> ListOfResponses = mapper.readValue(response.body(), new TypeReference<>() {
                 });
                 return new AdminAreaListResponse(ListOfResponses);
             } else {
---
+++ src/main/java/com/martintraverso/AccuWeather/Utilities/HttpRequestFactory.java
@@ -0,0 +1,31 @@
+package com.martintraverso.AccuWeather.Utilities;
+
+import com.martintraverso.AccuWeather.Request.AccuWeatherRequest;
+
+import java.net.URI;
+import java.net.http.HttpRequest;
+
+public abstract class HttpRequestFactory {
+    static String baseUrl = "http://dataservice.accuweather.com/";
+    static String apiKey = System.getenv("ACCUWEATHER_API_KEY");
+
+    public static HttpRequest create(AccuWeatherRequest accuWeatherRequest) throws Exception {
+
+        if (apiKey == null) {
+            throw new IllegalStateException("ACCUWEATHER_API_KEY environment variable is required");
+        }
+
+        String endpoint = baseUrl + accuWeatherRequest.getEndpoint() + "?apikey=" + apiKey;
+
+        if (accuWeatherRequest.getHttpMethod() == HttpMethods.GET) {
+            return HttpRequest.newBuilder()
+                    .uri(URI.create(endpoint))
+                    .GET()
+                    .header("Accept", "*/*")
+                    .header("Accept-Encoding", "identity")
+                    .header("Accept-Language", "es-ES")
+                    .build();
+        }
+        return null;
+    }
+}
